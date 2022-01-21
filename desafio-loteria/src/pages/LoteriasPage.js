1533cda06d3de207b5a4ceb771b49409cc90d28c
suphessel <suhessel28@gmail.com>
2022-01-21 03:23:07 -0300

    Estilização pronta

--- desafio-loteria/src/pages/LoteriasPage.js
+++ desafio-loteria/src/pages/LoteriasPage.js
@@ -2,16 +2,18 @@ import React, { useState, useEffect, useCallback } from "react";
 import {
   ContainerPage,
   ContainerLotery,
+  SelectLotery,
+  BoxContest,
   ContainerResult,
   TextBox,
   BoxResult,
   DivResult,
+  DivNumber,
 } from "./styled";
 import axios from "axios";
 import getInfoContest from "../services/concursos";
-import { getLoterias, getLoteriesContest } from "../services/loterias";
+import { getLoteriesContest } from "../services/loterias";
 import { BASE_URL, headers } from "../constants/urls";
-import useForm from "../hooks/useForm";
 
 const LoteriesPage = () => {
   const [loteries, setLoteries] = useState([]);
@@ -19,40 +21,26 @@ const LoteriesPage = () => {
   const [constestId, setConstestId] = useState("2359");
   const [contest, setContest] = useState({});
 
-  const nanana = useCallback( async () => {
-    const infos = await getInfoContest(constestId)
-    setContest(infos)
-    console.log("INFOS", infos)
-  }, [constestId], )
-  
+  const callbackContest = useCallback(async () => {
+    const infos = await getInfoContest(constestId);
+    setContest(infos);
+  }, [constestId]);
+
   useEffect(() => {
     getLoteries();
-    nanana()
-  }, [nanana]);
-
-
+    callbackContest();
+  }, [callbackContest]);
 
   const onChangeLotery = async (e) => {
-    console.log("e.target");
-    console.log(e.target.value);
-
     setLotery(e.target.value);
 
-    const relationships = await getLoteriesContest()
+    const relationships = await getLoteriesContest();
 
-    console.log(relationships, "relationships")
-
-   const newRelationship = relationships.filter((relationship) => {
-    console.log("relationship.loteriaId", relationship.loteriaId  )
-    console.log("e.target.value", e.target.value  )
+    const newRelationship = relationships.filter((relationship) => {
       return relationship.loteriaId === Number(e.target.value);
     });
 
-    console.log(newRelationship )
-    console.log("concursoId no onchange", typeof newRelationship.concursoId )
     setConstestId(newRelationship[0].concursoId);
-
-    console.log(newRelationship, "newRelationship")
   };
 
   const getLoteries = () => {
@@ -61,43 +49,45 @@ const LoteriesPage = () => {
         headers: headers,
       })
       .then((res) => {
-        console.log("res");
-        console.log(res.data);
-
         setLoteries(res.data);
       })
-      .catch((err) => {
-        console.log("ERROOO", err);
-      });
+      .catch((err) => {});
   };
 
   return (
     <ContainerPage>
       <ContainerLotery>
-        <select value={lotery} onChange={onChangeLotery}>
-          {loteries.map((lotery) => {
-            return <option value={lotery.id}>{lotery.nome}</option>;
-          })}
-        </select>
-        <div>
+        <SelectLotery>
+          <select value={lotery} onChange={onChangeLotery}>
+            {loteries.map((lotery) => {
+              return <option value={lotery.id}>{lotery.nome}</option>;
+            })}
+          </select>
+        </SelectLotery>
+        <BoxContest>
           <p>Concurso</p>
-          <p> {contest.id ?? "CARREGANDO!!"} - {contest.data ?? "CARREGANDO!!"}</p>
-        </div>
+          <p>
+            {" "}
+            {contest.id ?? "CARREGANDO!!"} - {contest.data ?? "CARREGANDO!!"}
+          </p>
+        </BoxContest>
       </ContainerLotery>
       <ContainerResult>
         <BoxResult>
-          <DivResult style={{ display: 'flex', flexDirection:"row", gap:"10px"}}>
-             { contest.numeros ? contest.numeros.map((numero) => {
-              return <div style={{backgroundColor: 'red', }}>{numero} </div>
-            }) : "CARREGANDO!!"}
+          <DivResult>
+            {contest.numeros
+              ? contest.numeros.map((numero) => {
+                  return <DivNumber>{numero} </DivNumber>;
+                })
+              : "CARREGANDO!!"}
           </DivResult>
+          <TextBox>
+            <p>
+              Este sorteio é meramente ilustrativo e não possui nenhuma ligação
+              com a CAIXA.
+            </p>
+          </TextBox>
         </BoxResult>
-        <TextBox>
-          <p>
-            Este sorteio é meramente ilustrativo e não possui nenhuma ligação
-            com a CAIXA.
-          </p>
-        </TextBox>
       </ContainerResult>
     </ContainerPage>
   );
--- desafio-loteria/src/pages/styled.js
+++ desafio-loteria/src/pages/styled.js
@@ -1,63 +1,133 @@
 import styled from "styled-components";
 
 export const ContainerPage = styled.div`
-display: flex;
-flex-direction: row;
-width: 100%;
-height: 100vh;
-background-color: pink;
-`
+  display: flex;
+  flex-direction: row;
+  width: 100%;
+  height: 100vh;
+  background-color: pink;
+`;
 
 export const ContainerLotery = styled.div`
-display: flex;
-flex-direction: column;
-width: 50%;
-height: auto;
-background-color: #6BEFA3;
+  display: flex;
+  flex-direction: column;
+  width: 40%;
+  height: auto;
+  background-color: #6befa3;
+  justify-content: space-between;
+  justify-items: center;
+  align-items: center;
+  align-content: center;
+  text-align: center; ;
+`;
 
-;
-`
+export const SelectLotery = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 50%;
+  height: auto;
+  background-color: #6befa3;
+  justify-items: center;
+  align-items: center;
+  align-content: center;
+  text-align: center;
+  padding-top: 35%;
+
+  select {
+    width: 100%;
+    padding: 5%;
+    text-align: center;
+    border-radius: 10px;
+    border: solid 1px white;
+    font-size: 16px;
+    color: #848484;
+  }
+`;
+export const BoxContest = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: auto;
+  background-color: #6befa3;
+  color: white;
+  font-weight: bold;
+  font-size: 15px;
+  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
+    Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
+  justify-content: center;
+  justify-items: center;
+  align-items: center;
+  align-content: center;
+  text-align: center;
+  padding-bottom: 10%;
+`;
 
 export const ContainerResult = styled.div`
-display: flex;
-flex-direction: column;
-width: 50%;
-height: auto;
-background-color:white;
- ;
-`
+  display: flex;
+  flex-direction: column;
+  width: 60%;
+  height: auto;
+  background-color: #6befa3; ;
+`;
 
 export const BoxResult = styled.div`
-display: flex;
-flex-direction: row;
-background-color: yellow;
-width: 100%;
-height: 80%;
-align-items: center;
-/* border-radius: 100px  0 0 100px; */
+  display: flex;
+  flex-direction: column;
+  background-color: #eee9e9;
+  width: 100%;
+  height: 100%;
+  align-items: center;
+  justify-content: center;
 
-
-`
+  align-items: flex-end;
+  text-align: flex-end;
+`;
 export const DivResult = styled.div`
-display: flex;
-flex-direction: column;
-background-color: blue;
-width: 100%;
-height: 50%;
-justify-content: center;
-justify-items: center;
-align-content: center;
-text-align: center;
+  display: flex;
+  flex-direction: row;
+  gap: 2%;
+  background-color: #eee9e9;
+  width: 100%;
+  height: 50%;
+  justify-content: center;
+  justify-items: center;
+  align-items: center;
+  align-content: center;
+  text-align: center;
+`;
 
-`
-
+export const DivNumber = styled.div`
+  display: flex;
+  width: 8%;
+  height: 20%;
+  background-color: white;
+  justify-content: center;
+  align-items: center;
+  border-radius: 60%;
+  /* border: solid 5px #cdc9c9; */
+  color: #333333;
+  font-size: 27px;
+  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
+    Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
+`;
 
 export const TextBox = styled.div`
-display: flex;
-flex-direction: row;
-background-color: pink;
-p{
+  display: flex;
+  flex-direction: row;
+  width: 90%;
+  height: 30%;
+  background-color: #eee9e9;
+  justify-content: center;
+  justify-items: center;
+  align-items: center;
+  align-content: center;
+  text-align: center;
 
-}
+  p {
+    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
+      Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
+    font-size: 12px;
 
-`
+    color: #333333;
+  }
+`;
